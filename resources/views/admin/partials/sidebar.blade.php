bda11363d44b8f9e675231ac9bbdd201475f41fc
AbdoallhReda <50003039+AbdoallhReda@users.noreply.github.com>
2023-10-10 11:10:43 +0300

    attribute

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -35,18 +35,25 @@
         </li>
 
         <li>
-            <a class="app-menu__item {{Route::currentRouteName() == 'admin.settings' ? 'active' :''}} "
-               href="{{route('admin.settings')}}">
-                <i class="app-menu__icon fa fa-cogs"></i>
-                <span class="app-menu__label">Settings</span>
-            </a>
-        </li>
-
-        <li>
             <a class="app-menu__item {{Route::currentRouteName() == 'admin.categories.index' ? 'active' :''}} "
                href="{{route('admin.categories.index')}}">
                 <i class="app-menu__icon fa fa-tags"></i>
                 <span class="app-menu__label">Categories</span>
+            </a>
+        </li>
+
+        <li>
+            <a class="app-menu__item {{ Route::currentRouteName() == 'admin.attributes.index' ? 'active' : '' }}" href="{{ route('admin.attributes.index') }}">
+                <i class="app-menu__icon fa fa-th"></i>
+                <span class="app-menu__label">Attributes</span>
+            </a>
+        </li>
+
+        <li>
+            <a class="app-menu__item {{Route::currentRouteName() == 'admin.settings' ? 'active' :''}} "
+               href="{{route('admin.settings')}}">
+                <i class="app-menu__icon fa fa-cogs"></i>
+                <span class="app-menu__label">Settings</span>
             </a>
         </li>
 
